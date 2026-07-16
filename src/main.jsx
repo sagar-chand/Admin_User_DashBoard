cb0d18bbb12a9bbed85428ea7f7033393d9b6c57
Sagar Chand Nayak <sagarchandnayak15@gmail.com>
2026-07-16 19:53:52 +0530

    Refactor Bootstrap imports in main.jsx
    Updated import paths for Bootstrap and Bootstrap Icons.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import 'node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import 'node_modules/bootstrap/dist/css/bootstrap.css';
+import 'node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import { VideoTutorialHome } from './video-tutorial-home.jsx';
 import { VideoTutorialIndex } from './video-tutorial-index.jsx';
 import { CookiesProvider } from 'react-cookie';
